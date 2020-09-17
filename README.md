b4bffc4a938e53ee9dce3ba3b71005dfc93678d6
Matthew Giaconelli <matthew.giaconelli@rbc.com>
2020-09-17 15:30:59 -0400

    tada

---
+++ README.md
@@ -0,0 +1,10 @@
+Usage.
+
+```bash
+npm run start <numUsers>
+```
+
+numUsers - optional, default 1000
+
+
+Get output in `users.csv` at root level.
--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-name,given_name,family_name,middle_name,nickname,preferred_username,profile,picture,website,email,email_verified,gender,birthdate,zoneinfo,locale,phone_number,phone_number_verified,address,updated_at,custom:last_login,custom:app_code,custom:compliance_id,custom:member_id,custom:activation_date,cognito:mfa_enabled,cognito:username
+const chance = require('chance')();
+const uuid = require('uuid');
+const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
+const fs = require('fs');
+
+const createUser = () => ({
+  name: chance.first(),
+  given_name: chance.first(),
+  family_name: chance.last(),
+  middle_name: chance.first(),
+  nickname: chance.first(),
+  preferred_username: chance.first(),
+  profile: chance.url(),
+  picture: chance.url(),
+  website: chance.url(),
+  email: chance.email(),
+  email_verified: true,
+  gender: chance.gender(),
+  birthdate: chance.date(),
+  zoneinfo: null,
+  locale: chance.locale(),
+  phone_number: `+1${chance.phone({ formatted: false })}`,
+  phone_number_verified: true,
+  address: chance.address(),
+  updated_at: null,
+  'custom:last_login': null,
+  'custom:app_code': 'TUV0',
+  'custom:compliance_id': uuid.v4(),
+  'custom:member_id': uuid.v4(),
+  'custom:activation_date': chance.date(),
+  'cognito:mfa_enabled': false,
+  'cognito:username': `${chance.first()}${chance.last()}@mailinator.com`.trim(),
+});
+
+const getHeaders = () => [...Object.keys(createUser())].map((elem) => ({ id: elem, title: elem }));
+
+const usersToGenerate = process.argv[2] || 1000;
+
+const header = getHeaders();
+const csvStringifier = createCsvStringifier({
+  header,
+});
+const fsWriteStream = fs.createWriteStream('users.csv');
+fsWriteStream.write(csvStringifier.getHeaderString());
+
+for (let i = 0; i < usersToGenerate; i += 1) {
+  fsWriteStream.write(csvStringifier.stringifyRecords(createUser()));
+}
